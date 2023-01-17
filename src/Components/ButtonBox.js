b05f4fc2122a7aa4245a0376e19884ce5c01a38c
Annemarie <a.lucernoni0@gmail.com>
2023-01-17 12:28:27 -0700

    initial buttonbox container css, may need adjustment

--- src/Components/ButtonBox.js
+++ src/Components/ButtonBox.js
@@ -2,7 +2,11 @@ import "./ButtonBox.css"
 import React from "react"
 
 function ButtonBox() {
+    return (
+        <div className="buttonbox">
 
+        </div>
+    )
 }
 
 export default ButtonBox
