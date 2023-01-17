cc356d4f88360fe36c8c90113144130e4e09fd61
Annemarie <a.lucernoni0@gmail.com>
2023-01-17 14:14:33 -0700

    buttons grid added with screen default display

--- src/Components/ButtonBox.js
+++ src/Components/ButtonBox.js
@@ -1,10 +1,34 @@
 import "./ButtonBox.css"
 import React from "react"
+import Button from "./Button"
 
 function ButtonBox() {
+
+    const buttonValues = [
+        ["C", "+/-", "%", "/"],
+        [7, 8, 9, "X"],
+        [4, 5, 6, "-"],
+        [1, 2, 3, "+"],
+        [0, ".", "="]
+    ]
+
     return (
         <div className="buttonbox">
-
+            {
+                buttonValues.flat().map((btn, i) => {
+                    return (
+                        <Button
+                            key={i}
+                            className={btn === "=" ? "equals" : ""}
+                            value={btn}
+                            onClick={() => {
+                                console.log(`${btn} clicked!`)
+                            }}
+                        />
+                    )
+                })
+            }
+            {/* <Button/> */}
         </div>
     )
 }
